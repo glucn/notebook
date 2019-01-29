dbc18d2ceb02cd06efc10597dd5045ce98619e61
glucn <glu@vendasta.com>
2019-01-28 21:47:19 -0600

    use loader to load data in a separate thread

--- app/src/main/java/xyz/glucn/notebook/MainActivity.java
+++ app/src/main/java/xyz/glucn/notebook/MainActivity.java
@@ -8,11 +8,19 @@ import android.util.Log;
 import android.widget.CursorAdapter;
 import android.widget.SimpleCursorAdapter;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+implements LoaderManager.LoaderCallbacks<Cursor>
+{
+    private CursorAdapter mCursorAdapter;
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
@@ -22,13 +30,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        insertNote("New Note");
+        // insertNote("New Note");
 
-        Cursor cursor = getContentResolver().query(NotesProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS, null, null, null);
         String[] from = {DBOpenHelper.NOTE_TEXT};
         int[] to = {android.R.id.text1};
 
-        CursorAdapter cursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, cursor, from, to, 0);
+        mCursorAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_1, null, from, to, 0);
         mRecyclerView = findViewById(R.id.note_recycle_view);
 
         mRecyclerView.setHasFixedSize(true);
@@ -36,8 +43,10 @@ public class MainActivity extends AppCompatActivity {
         mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
 
-        mAdapter = new MyRecyclerAdapter(this, cursorAdapter);
+        mAdapter = new MyRecyclerAdapter(this, mCursorAdapter);
         mRecyclerView.setAdapter(mAdapter);
+
+        LoaderManager.getInstance(this).initLoader(0, null, this);
 
     }
 
@@ -46,5 +55,22 @@ public class MainActivity extends AppCompatActivity {
         values.put(DBOpenHelper.NOTE_TEXT, note);
         Uri uri = getContentResolver().insert(NotesProvider.CONTENT_URI, values);
         Log.i("MainActivity", "onCreate: data inserted " + uri.getLastPathSegment());
+    }
+
+    @NonNull
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(this, NotesProvider.CONTENT_URI, DBOpenHelper.ALL_COLUMNS,
+                null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader<Cursor> loader, Cursor data) {
+        mCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader<Cursor> loader) {
+        mCursorAdapter.swapCursor(null);
     }
 }
